93ac86cba4d93a9a0fb8d04cf7284bd5d916c704
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-03-17 12:16:06 -0300

    Atualizando design do projeto

--- SimplePlan/Form1.Designer.cs
+++ SimplePlan/Form1.Designer.cs
@@ -61,34 +61,31 @@ namespace SimplePlan
             // 
             // buttonLimpar
             // 
-            this.buttonLimpar.Location = new System.Drawing.Point(523, 25);
+            this.buttonLimpar.Location = new System.Drawing.Point(570, 23);
             this.buttonLimpar.Name = "buttonLimpar";
-            this.buttonLimpar.Size = new System.Drawing.Size(95, 25);
+            this.buttonLimpar.Size = new System.Drawing.Size(95, 40);
             this.buttonLimpar.TabIndex = 7;
             this.buttonLimpar.Text = "Limpar";
-            this.buttonLimpar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.buttonLimpar.UseVisualStyleBackColor = true;
             this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
             // 
             // buttonSalvar
             // 
-            this.buttonSalvar.Location = new System.Drawing.Point(711, 27);
+            this.buttonSalvar.Location = new System.Drawing.Point(695, 25);
             this.buttonSalvar.Name = "buttonSalvar";
-            this.buttonSalvar.Size = new System.Drawing.Size(76, 23);
+            this.buttonSalvar.Size = new System.Drawing.Size(76, 38);
             this.buttonSalvar.TabIndex = 6;
             this.buttonSalvar.Text = "Salvar";
-            this.buttonSalvar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.buttonSalvar.UseVisualStyleBackColor = true;
             this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
             // 
             // buttonBorracha
             // 
-            this.buttonBorracha.Location = new System.Drawing.Point(409, 25);
+            this.buttonBorracha.Location = new System.Drawing.Point(455, 23);
             this.buttonBorracha.Name = "buttonBorracha";
-            this.buttonBorracha.Size = new System.Drawing.Size(87, 25);
+            this.buttonBorracha.Size = new System.Drawing.Size(87, 40);
             this.buttonBorracha.TabIndex = 4;
             this.buttonBorracha.Text = "Borracha";
-            this.buttonBorracha.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.buttonBorracha.UseVisualStyleBackColor = true;
             this.buttonBorracha.Click += new System.EventHandler(this.buttonBorracha_Click);
             this.buttonBorracha.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonBorracha_MouseDown);
@@ -97,7 +94,7 @@ namespace SimplePlan
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(185, 19);
+            this.label2.Location = new System.Drawing.Point(285, 11);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(164, 18);
             this.label2.TabIndex = 3;
@@ -106,7 +103,7 @@ namespace SimplePlan
             // CbExpressuraCaneta
             // 
             this.CbExpressuraCaneta.FormattingEnabled = true;
-            this.CbExpressuraCaneta.Location = new System.Drawing.Point(204, 40);
+            this.CbExpressuraCaneta.Location = new System.Drawing.Point(309, 32);
             this.CbExpressuraCaneta.Name = "CbExpressuraCaneta";
             this.CbExpressuraCaneta.Size = new System.Drawing.Size(121, 26);
             this.CbExpressuraCaneta.TabIndex = 2;
@@ -117,9 +114,9 @@ namespace SimplePlan
             this.buttonCorCaneta.BackColor = System.Drawing.Color.LightGreen;
             this.buttonCorCaneta.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonCorCaneta.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
-            this.buttonCorCaneta.Location = new System.Drawing.Point(37, 39);
+            this.buttonCorCaneta.Location = new System.Drawing.Point(31, 29);
             this.buttonCorCaneta.Name = "buttonCorCaneta";
-            this.buttonCorCaneta.Size = new System.Drawing.Size(60, 26);
+            this.buttonCorCaneta.Size = new System.Drawing.Size(54, 40);
             this.buttonCorCaneta.TabIndex = 1;
             this.buttonCorCaneta.UseVisualStyleBackColor = false;
             this.buttonCorCaneta.Click += new System.EventHandler(this.buttonCorCaneta_Click);
@@ -128,7 +125,7 @@ namespace SimplePlan
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Location = new System.Drawing.Point(6, 8);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(111, 18);
             this.label1.TabIndex = 0;
@@ -160,7 +157,7 @@ namespace SimplePlan
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Desenho Simples";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
